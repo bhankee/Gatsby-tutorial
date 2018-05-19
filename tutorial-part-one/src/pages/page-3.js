a215cefd836d0253178eb8ee9a88c0a2151ad958
Brad Hankee <brad.hankee@gmail.com>
2018-05-18 20:58:07 -0400

    Made static site and published via Surge.

---
+++ tutorial-part-one/src/pages/page-3.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+export default () => (
+  <div>
+    <p>Hankee's Third Gatsby Page</p>
+    <Link to="/">back home</Link>
+  </div>
+);
